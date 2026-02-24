9f4f867072f97acb697710ee0b709bc63975b21a
Sarthak Girme <sarthak.girme18@gmail.com>
2026-02-24 14:49:39 +0530

    core: two-way-binding using signals

--- web-client/src/app/shared/components/new-task/new-task.componenet.ts
+++ web-client/src/app/shared/components/new-task/new-task.componenet.ts
@@ -1,14 +1,21 @@
-import { Component, output } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
+  imports: [ FormsModule ],
   templateUrl: './new-task.componenet.html',
   styleUrl: './new-task.componenet.css',
 })
 export class NewTaskComponenet {
 
   cancel = output<void>();
+
+  protected enteredTitle = signal<string>('');
+  protected enteredSummary = signal<string>('');
+  protected enteredDueDate = signal<string>('');
 
   onCancel(): void {
     this.cancel.emit();
