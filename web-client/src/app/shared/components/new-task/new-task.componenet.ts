3df5529429f29c3975a505bfe42038c3548034ce
Sarthak Girme <sarthak.girme18@gmail.com>
2026-02-24 15:28:40 +0530

    core: form submission and UI update

--- web-client/src/app/shared/components/new-task/new-task.componenet.ts
+++ web-client/src/app/shared/components/new-task/new-task.componenet.ts
@@ -1,6 +1,7 @@
 import { Component, output, signal } from '@angular/core';
-
 import { FormsModule } from '@angular/forms';
+
+import { type NewTask } from '../../../core/models/task.model';
 
 @Component({
   selector: 'app-new-task',
@@ -12,13 +13,22 @@ import { FormsModule } from '@angular/forms';
 export class NewTaskComponenet {
 
   cancel = output<void>();
+  add = output<NewTask>();
 
   protected enteredTitle = signal<string>('');
   protected enteredSummary = signal<string>('');
   protected enteredDueDate = signal<string>('');
 
-  onCancel(): void {
+  onCancel() :void {
     this.cancel.emit();
+  }
+
+  onSubmit() :void {
+    this.add.emit({
+      title: this.enteredTitle(),
+      summary: this.enteredSummary(),
+      dueDate: this.enteredDueDate()
+    });
   }
 
 }
--- web-client/src/app/shared/components/task-list/task-list.component.ts
+++ web-client/src/app/shared/components/task-list/task-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, input, computed, signal } from '@angular/core';
 
 import { type User } from '../../../core/models/user.model';
+import { type NewTask } from '../../../core/models/task.model';
 import { DUMMY_TASKS } from '../../../testing/mocks/task.mock';
 
 import { TaskDetailComponent } from '../task-detail/task-detail.component';
@@ -40,4 +41,22 @@ export class TaskListComponent {
   protected onCancelAddTaskEvent() : void {
     this.isAddingTask = false;
   }
+
+  protected onAddEvent(newTask: NewTask) :void {
+    
+    this.tasks.update(
+      currentTask => [ ...currentTask, {
+          id: new Date().getTime().toString(),
+          userId: this.user().id,
+          title: newTask.title,
+          summary: newTask.summary,
+          dueDate: newTask.dueDate
+        }
+      ]
+    );
+
+    // Close pop-up of AddTaskComponenet
+    this.isAddingTask = false; 
+  }
+
 }
